9f9e27317555f5aef4b7ed35b25b9466a514f7d7
Sou1nce <119765391+Sou1nce@users.noreply.github.com>
2024-04-08 11:35:00 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,31 +3,31 @@ function toggleNavigation() {
     var menuButton = document.getElementById('menu-button');
     var menu = document.getElementById('menu');
 
-    // Если экран меньше или равен 1199px, скрываем навигацию и показываем кнопку меню
+   
     if (window.innerWidth <= 1199) {
         navigation.style.display = 'none';
         menuButton.style.display = 'block';
     } else {
-        // Иначе показываем навигацию и скрываем кнопку меню
+      
         navigation.style.display = 'block';
         menuButton.style.display = 'none';
-        // Также скрываем меню, если оно открыто
+ 
         menu.style.display = 'none';
     }
 }
 
-// Вызываем функцию при загрузке страницы и при изменении размера окна
+
 window.onload = toggleNavigation;
 window.onresize = toggleNavigation;
 
-// Обработчик клика по кнопке открытия меню
+
 document.getElementById('menu-button').addEventListener('click', function() {
     document.getElementById('menu').style.display = 'block';
     document.getElementById('menu-button').style.display = 'none'
 
 });
 
-// Обработчик клика по кнопке закрытия меню
+
 document.getElementById('close-menu-button').addEventListener('click', function() {
     document.getElementById('menu').style.display = 'none';
     document.getElementById('menu-button').style.display = 'block'
